06e3d06d4b21761dd91c09b40a0eec0d86162fa9
Gleison Amorim <54336609+GleisonAmorim@users.noreply.github.com>
2025-05-03 13:16:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def download_video():
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    port = int(os.genv('PORT'),'5000')
+    port = int(os.environ.get('PORT', 5000))
     app.run(host= '0.0.0.0', port = port)
